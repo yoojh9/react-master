dfebcded931d508508f3e0cb109f3c3aa1807f10
yoojh9 <yoojh9@gmail.com>
2021-12-14 09:00:33 +0900

    add coin data

--- crypto-tracker/src/routes/Coin.tsx
+++ crypto-tracker/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
@@ -36,7 +36,20 @@ function Coin() {
     const { coinId } = useParams<RouteParams>();
     const [loading, setLoading] = useState(true);
     const { state } = useLocation<RouteState>();
-
+    const [info, setInfo] = useState({});
+    const [priceInfo, setPriceInfo] = useState({});
+    useEffect(() => {
+        (async () => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+            ).json();
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+            ).json()
+            setInfo(infoData);
+            setPriceInfo(priceData);
+        })();
+    }, [])
     return <Container>
         <Header>
             <Title>{state?.name || "Loading.."}</Title>
